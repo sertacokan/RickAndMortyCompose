7507b6d3e5abbae1d8de9890a57e54bc0e163138
Sertac Okan Celik <sertacokan@gmail.com>
2021-10-20 01:21:50 +0300

    Refactor EqualSizedItemRow composable child constraints

--- app/src/main/java/com/example/rickandmortycompose/views/EqualSizedItemRow.kt
+++ app/src/main/java/com/example/rickandmortycompose/views/EqualSizedItemRow.kt
@@ -1,37 +1,31 @@
 package com.example.rickandmortycompose.views
 
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.unit.dp
-import kotlin.math.min
 
 @Composable
 fun EqualSizedItemRow(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
-    Layout(
-        content = content,
-        modifier = modifier
-    ) { measurableList, constraints ->
-        val spaceWidthPx = 5.dp.roundToPx()
-        val minItemWidthPx = (100.dp).roundToPx()
+    // measurables is the list of children that need to be measured and constraints are the constraints from the parent.
+    Layout(content = content, modifier = modifier) { measurableList, constraints ->
         val itemCount = measurableList.size
+        val spaceWidthPx = 5.dp.roundToPx()
         val totalSpace = (itemCount - 1) * spaceWidthPx
         val itemSize = (constraints.maxWidth - totalSpace) / itemCount
-        val minWidthPx = min(itemSize, minItemWidthPx)
-        val placeableList = measurableList.map { measurable ->
-            measurable.measure(constraints.copy(minWidth = minWidthPx, maxWidth = minWidthPx)) // TODO : Fix width issue and show background color
-        }
+
+        val childConstraint = constraints.copy(maxWidth = itemSize, minWidth = 0)
+
+        // TODO : Fix width issue and show background color
+        val placeableList = measurableList.map { measurable -> measurable.measure(childConstraint) }
+
         layout(constraints.maxWidth, constraints.minHeight) {
             var startX = 0
             placeableList.forEachIndexed { position, placeable ->
-                placeable.place(x = startX, y = 0)
+                placeable.placeRelative(x = startX, y = 0)
                 startX += if (position != placeableList.lastIndex) placeable.width + spaceWidthPx else placeable.width
             }
         }
