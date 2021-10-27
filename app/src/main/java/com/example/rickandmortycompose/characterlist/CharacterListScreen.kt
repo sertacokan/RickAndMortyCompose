d4c0d40654e46fb30ad63817ee1e2f066d74f740
Sertac Okan Celik <sertacokan@gmail.com>
2021-10-27 18:32:56 +0300

    Refactor CharacterListItem with flow row

--- app/src/main/java/com/example/rickandmortycompose/characterlist/CharacterListScreen.kt
+++ app/src/main/java/com/example/rickandmortycompose/characterlist/CharacterListScreen.kt
@@ -5,18 +5,31 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.rickandmortycompose.components.SearchBar
-import com.example.rickandmortycompose.network.response.Character
+import org.koin.androidx.compose.getViewModel
 
 @Composable
-fun CharacterListScreen(characters: List<Character>) {
-    val (query, onValueChanged) = remember { mutableStateOf("") }
-    Column(modifier = Modifier.fillMaxWidth()) {
-        SearchBar(query = query, onQueryChanged = onValueChanged)
+fun CharacterListScreen(characterListViewModel: CharacterListViewModel = getViewModel()) {
+    val characters by characterListViewModel.characterList.collectAsState()
+    val query by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        SearchBar(query = query, onQueryChanged = { query -> characterListViewModel.searchCharacter(query = query) })
         LazyColumn {
             items(characters) { character ->
-                CharacterListItem(character = character, onCharacterItemClicked = {})
+                CharacterListItem(character = character) {}
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun CharacterListScreenPreview() {
+    CharacterListScreen()
 }
