a95a9db092d784e6ec3ab7e5b8f18ede69d62016
Sertac Okan Celik <sertacokan@gmail.com>
2021-10-27 23:02:41 +0300

    Add characterId parameter on CharacterDetailScreen

--- app/src/main/java/com/example/rickandmortycompose/characterdetail/CharacterDetailScreen.kt
+++ app/src/main/java/com/example/rickandmortycompose/characterdetail/CharacterDetailScreen.kt
@@ -3,6 +3,6 @@ package com.example.rickandmortycompose.characterdetail
 import androidx.compose.runtime.Composable
 
 @Composable
-fun CharacterDetailScreen() {
+fun CharacterDetailScreen(characterId: Int) {
 
 }
