4ed78e4a34dd3b2e53ee9cd51edeab6937569fb3
Sertac Okan Celik <sertacokan@gmail.com>
2021-10-31 23:27:53 +0300

    Add paging 3 library and implement dependency graph

--- app/src/main/java/com/example/rickandmortycompose/characterlist/CharacterListViewModel.kt
+++ app/src/main/java/com/example/rickandmortycompose/characterlist/CharacterListViewModel.kt
@@ -1,32 +1,33 @@
 package com.example.rickandmortycompose.characterlist
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import com.example.rickandmortycompose.network.CharacterRepository
-import com.example.rickandmortycompose.network.response.Character
+import com.example.rickandmortycompose.paging.CharacterDataSource
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.flatMapLatest
 
-class CharacterListViewModel(private val characterRepository: CharacterRepository) : ViewModel() {
-
-    private val _characterList = MutableStateFlow(emptyList<Character>())
-    val characterList: StateFlow<List<Character>> = _characterList
+class CharacterListViewModel(private val characterRepository: CharacterRepository, private val pagingConfig: PagingConfig) : ViewModel() {
 
     private val _queryText = MutableStateFlow("")
     val queryText: StateFlow<String> = _queryText
 
-    init {
-        fetchCharacterList()
-    }
+    val characterListPagingData = _queryText
+        .debounce(QUERY_DEBOUNCE)
+        .flatMapLatest { query ->
+            Pager(config = pagingConfig) {
+                CharacterDataSource(characterRepository = characterRepository, query = query)
+            }.flow
+        }
 
     fun searchCharacter(query: String) {
         _queryText.value = query
     }
 
-    private fun fetchCharacterList() {
-        viewModelScope.launch {
-            _characterList.value = characterRepository.getCharacterList(pageNumber = 1)
-        }
+    companion object {
+        private const val QUERY_DEBOUNCE = 300L
     }
 }
---
+++ app/src/main/java/com/example/rickandmortycompose/paging/CharacterDataSource.kt
@@ -0,0 +1,32 @@
+package com.example.rickandmortycompose.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.rickandmortycompose.network.CharacterRepository
+import com.example.rickandmortycompose.network.response.Character
+import java.lang.Exception
+
+class CharacterDataSource(private val characterRepository: CharacterRepository, private val query: String? = null) : PagingSource<Int, Character>() {
+
+    override fun getRefreshKey(state: PagingState<Int, Character>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition = anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Character> {
+        return try {
+            val pageNumber = params.key ?: 1
+            val characterResponse = if (query.isNullOrBlank()) {
+                characterRepository.fetchCharacterList(pageNumber = pageNumber)
+            } else {
+                characterRepository.fetchCharacterListByName(name = query, pageNumber = pageNumber)
+            }
+            val nextPageNumber = if (pageNumber < characterResponse.info.pages) pageNumber + 1 else null
+            LoadResult.Page(data = characterResponse.results, prevKey = null, nextKey = nextPageNumber)
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+}
