aff825a25adbc9b00782c71069a90cbea631056c
Sertac Okan Celik <sertacokan@gmail.com>
2021-11-01 23:09:05 +0300

    Add Serializable annotation to response models

--- app/src/main/java/com/example/rickandmortycompose/network/response/CharacterResponse.kt
+++ app/src/main/java/com/example/rickandmortycompose/network/response/CharacterResponse.kt
@@ -1,5 +1,8 @@
 package com.example.rickandmortycompose.network.response
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class CharacterResponse(
     val info: ResponseInfo,
     val results: List<Character>
