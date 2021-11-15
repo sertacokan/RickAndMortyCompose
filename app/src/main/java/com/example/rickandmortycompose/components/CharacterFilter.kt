8eb7273094ea1323d921924ee428eae82b976625
Sertac Okan Celik <sertacokan@gmail.com>
2021-11-15 14:27:06 +0300

    Remove SearchBar from CharacterListScreen and start character filter creation

---
+++ app/src/main/java/com/example/rickandmortycompose/components/CharacterFilter.kt
@@ -0,0 +1,12 @@
+package com.example.rickandmortycompose.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CharacterFilter(modifier: Modifier = Modifier) {
+    Row(modifier = modifier) {
+
+    }
+}
