70dee083b434ab801b11d48fba2280b4a08d4a7d
Sertac Okan Celik <sertacokan@gmail.com>
2021-11-15 14:21:06 +0300

    Convert CharacterRepository methods to retrofit and rename methods

--- app/src/main/java/com/example/rickandmortycompose/network/CharacterRepository.kt
+++ app/src/main/java/com/example/rickandmortycompose/network/CharacterRepository.kt
@@ -5,15 +5,15 @@ import com.example.rickandmortycompose.network.response.CharacterResponse
 
 class CharacterRepository(private val characterService: CharacterService) {
 
-    suspend fun getCharacterList(): List<Character> {
-        return httpClient.get {
-            addPathNodes("")
-        }
+    suspend fun fetchCharacterList(pageNumber: Int): CharacterResponse {
+        return characterService.fetchCharacterList(pageNumber = pageNumber)
     }
 
-    suspend fun getCharacterInfo(characterId: String): Character {
-        return httpClient.get {
-            addPathNodes("")
-        }
+    suspend fun fetchCharacterInfo(characterId: String): Character {
+        return characterService.fetchCharacterInfoById(characterId = characterId)
+    }
+
+    suspend fun fetchCharacterListByName(name: String, pageNumber: Int): CharacterResponse {
+        return characterService.fetchCharacterListByName(name = name, pageNumber = pageNumber)
     }
 }
