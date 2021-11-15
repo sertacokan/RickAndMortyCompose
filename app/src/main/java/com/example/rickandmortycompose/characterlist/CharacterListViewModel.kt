7ca3f4f21229424b73b8b124e4f1d8f89a6d3be4
Sertac Okan Celik <sertacokan@gmail.com>
2021-11-15 14:27:06 +0300

    Remove SearchBar from CharacterListScreen and start character filter creation

--- app/src/main/java/com/example/rickandmortycompose/characterlist/CharacterListViewModel.kt
+++ app/src/main/java/com/example/rickandmortycompose/characterlist/CharacterListViewModel.kt
@@ -9,29 +9,17 @@ import com.example.rickandmortycompose.network.CharacterRepository
 import com.example.rickandmortycompose.paging.CharacterDataSource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.flatMapLatest
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class CharacterListViewModel(private val characterRepository: CharacterRepository) : ViewModel() {
 
     private val _queryText = MutableStateFlow("")
-    val queryText: StateFlow<String> = _queryText
 
     val characterListPagingData = _queryText
-        .debounce(QUERY_DEBOUNCE)
         .flatMapLatest { query ->
             Pager(config = PagingConfig(pageSize = 20, enablePlaceholders = false), initialKey = 1) {
                 CharacterDataSource(characterRepository = characterRepository, query = query)
             }.flow.cachedIn(viewModelScope)
         }
-
-    fun searchCharacter(query: String) {
-        _queryText.value = query
-    }
-
-    companion object {
-        private const val QUERY_DEBOUNCE = 300L
-    }
 }
