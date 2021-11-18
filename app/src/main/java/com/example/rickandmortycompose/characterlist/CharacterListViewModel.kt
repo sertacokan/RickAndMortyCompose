4f309b6749263b4f25efce52e33ab3df4cbe284a
Sertac Okan Celik <sertacokan@gmail.com>
2021-11-18 13:41:46 +0300

    Remove query text flow

--- app/src/main/java/com/example/rickandmortycompose/characterlist/CharacterListViewModel.kt
+++ app/src/main/java/com/example/rickandmortycompose/characterlist/CharacterListViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.rickandmortycompose.characterlist
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
@@ -8,24 +9,17 @@ import androidx.paging.cachedIn
 import com.example.rickandmortycompose.network.CharacterRepository
 import com.example.rickandmortycompose.paging.CharacterDataSource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.*
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class CharacterListViewModel(private val characterRepository: CharacterRepository) : ViewModel() {
 
-    private val _queryText = MutableStateFlow("")
     private val _filters = MutableStateFlow(mutableListOf<String>())
-     val filters: StateFlow<MutableList<String>> = _filters
+    val filters: StateFlow<MutableList<String>> = _filters
 
-    val characterListPagingData = _queryText
-        .flatMapLatest { query ->
-            Pager(config = PagingConfig(pageSize = 20, enablePlaceholders = false), initialKey = 1) {
-                CharacterDataSource(characterRepository = characterRepository, query = query)
-            }.flow.cachedIn(viewModelScope)
-        }
+    val characterListPagingData = Pager(config = PagingConfig(pageSize = 20, enablePlaceholders = false), initialKey = 1) {
+        CharacterDataSource(characterRepository = characterRepository)
+    }.flow.cachedIn(viewModelScope)
 
     fun addFilter(filter: String) {
         _filters.update { filters ->
