c18877eee937d4b2da84f5edf6279c2646011bde
Sertac Okan Celik <sertacokan@gmail.com>
2021-11-18 12:23:24 +0300

    Add prev and next page url fields to CharacterResponse.kt and refactor data source by next page url

--- app/src/main/java/com/example/rickandmortycompose/network/response/CharacterResponse.kt
+++ app/src/main/java/com/example/rickandmortycompose/network/response/CharacterResponse.kt
@@ -3,5 +3,7 @@ package com.example.rickandmortycompose.network.response
 
 data class CharacterResponse(
     val info: ResponseInfo,
+    val next: String? = null,
+    val prev: String? = null,
     val results: List<Character>
 )
