ae43b567b843477ff01c1c18cabb85c4139d7214
Sertac Okan Celik <sertacokan@gmail.com>
2021-11-29 22:41:54 +0300

    Remove CharacterFilter section list usage

--- app/src/main/java/com/example/rickandmortycompose/characterlist/CharacterListScreen.kt
+++ app/src/main/java/com/example/rickandmortycompose/characterlist/CharacterListScreen.kt
@@ -7,10 +7,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
-import com.example.rickandmortycompose.R
 import com.example.rickandmortycompose.characterfilter.CharacterFilter
 import com.example.rickandmortycompose.characterfilter.rememberCharacterFilterState
-import com.example.rickandmortycompose.components.FilterSection
 import com.example.rickandmortycompose.network.response.Character
 import org.koin.androidx.compose.getViewModel
 
@@ -23,11 +21,6 @@ fun CharacterListScreen(
     val lazyCharacterItems =
         characterListViewModel.characterListPagingData.collectAsLazyPagingItems()
 
-    val sectionList = listOf(
-        FilterSection(sectionTitleRes = R.string.character_gender_title, listOf("female","male","genderless","unknown")),
-        FilterSection(sectionTitleRes = R.string.character_status_title, listOf("alive","dead","unknown"))
-    )
-
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -35,15 +28,14 @@ fun CharacterListScreen(
     ) {
         CharacterFilter(
             filterState = characterFilterState,
-            filterSection = sectionList,
             onFilterExpandClick = { isExpanded ->
                 characterFilterState.expanded.value = isExpanded
             },
             onFilterChipSelect = { appliedFilter ->
-               characterFilterState.addFilter(appliedFilter)
+                characterFilterState.addFilter(appliedFilter)
             },
             onFilterChipClose = { closedFilter ->
-               characterFilterState.removeFilter(closedFilter)
+                characterFilterState.removeFilter(closedFilter)
             }
         )
         LazyColumn(modifier = Modifier.fillMaxSize()) {
